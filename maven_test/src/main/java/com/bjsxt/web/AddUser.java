4b972633cce60b33911931b902f0226ddbc4bc68
songjh <songjh>
2018-12-10 11:44:40 +0800

    第一次

---
+++ maven_test/src/main/java/com/bjsxt/web/AddUser.java
@@ -0,0 +1,25 @@
+package com.bjsxt.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Auther: 宋甲贺
+ * @Date: 2018/12/10
+ * @Description: ${PACKAGE_NAME}
+ * @version: 1.0
+ */
+public class AddUser extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String username=request.getParameter("username");
+        System.out.println(username);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request,response);
+    }
+}
---
+++ maven_test/src/main/java/com/bjsxt/web/UserServlet.java
@@ -0,0 +1,14 @@
+package com.bjsxt.web;
+
+/**
+ * @Auther: 宋甲贺
+ * @Date: 2018/12/10
+ * @Description: com.bjsxt.web
+ * @version: 1.0
+ */
+public class UserServlet {
+    public String addUser(String username){
+
+        return "username:"+username;
+    }
+}
